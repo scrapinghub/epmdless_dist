db29b50d20a1211f356eca3809ac775133d7f76d
Dmitriy Mazurin <dimas.m.19@gmail.com>
2017-07-04 12:16:02 +0100

    Add simple module with API wrapper. Rename app module

--- src/epmdless_dist.erl
+++ src/epmdless_dist.erl
@@ -1,18 +1,38 @@
 -module(epmdless_dist).
 
--behaviour(application).
-
--export([start/0]).
--export([start/2, stop/1]).
-
-
-start() ->
-    application:start(?MODULE).
+-export([add_node/2]).
+-export([remove_node/1]).
+-export([list_nodes/0]).
+-export([set_nodes/1]).
 
 
-start(_StartType, _StartArgs) ->
-    epmdless_dist_sup:start_link().
+-spec add_node(Node, Port) -> ok when
+      Node :: atom(),
+      Port :: inet:port_number().
+add_node(Node, Port) ->
+    epmdless_client:add_node(Node, Port).
 
 
-stop(_State) ->
+
+-spec remove_node(Node) -> ok when
+      Node :: atom().
+remove_node(Node) ->
+    epmdless_client:remove_node(Node).
+
+
+-spec list_nodes() -> [{Node, Port}] when
+      Node :: atom(),
+      Port :: inet:port_number().
+list_nodes() ->
+    epmdless_client:list_nodes().
+
+
+
+-spec set_nodes(Nodes) -> ok when
+      Nodes :: [{Node, Port}],
+      Node  :: atom(),
+      Port  :: inet:port_number().
+set_nodes(Nodes) ->
+    _ = [remove_node(Node) || {Node, _} <- list_nodes()],
+    _ = [add_node(Node, Port) || {Node, Port} <- Nodes],
     ok.
